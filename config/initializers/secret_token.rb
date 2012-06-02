152d7423addf5b2fa50b0dbf37eab1e867f5cb88
freshx <trograv@hotmail.fr>
2012-06-02 14:52:39 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SupCV::Application.config.secret_token = '5caef17d538c9898a788900d7fbad6e0bf27c4edb890705918fa870eef7fde958f4fa66b6d7599222ce0dc845a60718cdc88d91d46b9e578f3823f3785cd8ac1'
